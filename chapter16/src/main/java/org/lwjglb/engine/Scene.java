dc6d16f5fa1a1a376a6eff84255bf169fabf39c5
lwjglgamedev <ahbejarano@gmail.com>
2015-12-16 19:25:10 +0100

    Code for Chapter 16 (Fog)

---
+++ chapter16/src/main/java/org/lwjglb/engine/Scene.java
@@ -0,0 +1,83 @@
+package org.lwjglb.engine;
+
+import org.lwjglb.engine.items.SkyBox;
+import org.lwjglb.engine.items.GameItem;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.lwjglb.engine.graph.Mesh;
+import org.lwjglb.engine.graph.weather.Fog;
+
+public class Scene {
+
+    private Map<Mesh, List<GameItem>> meshMap;
+    
+    private GameItem[] gameItems;
+    
+    private SkyBox skyBox;
+    
+    private SceneLight sceneLight;
+
+    private Fog fog;
+    
+    public Scene() {
+        meshMap = new HashMap();
+        fog = Fog.NOFOG;
+    }
+    
+    public Map<Mesh, List<GameItem>> getGameMeshes() {
+        return meshMap;
+    }
+
+    public void setGameItems(GameItem[] gameItems) {
+        int numGameItems = gameItems != null ? gameItems.length : 0;
+        for (int i=0; i<numGameItems; i++) {
+            GameItem gameItem = gameItems[i];
+            Mesh mesh = gameItem.getMesh();
+            List<GameItem> list = meshMap.get(mesh);
+            if ( list == null ) {
+                list = new ArrayList<>();
+                meshMap.put(mesh, list);
+            }
+            list.add(gameItem);
+        }
+    }
+
+    public void cleanup() {
+        for (Mesh mesh : meshMap.keySet()) {
+            mesh.cleanUp();
+        }
+    }
+
+    public SkyBox getSkyBox() {
+        return skyBox;
+    }
+
+    public void setSkyBox(SkyBox skyBox) {
+        this.skyBox = skyBox;
+    }
+
+    public SceneLight getSceneLight() {
+        return sceneLight;
+    }
+
+    public void setSceneLight(SceneLight sceneLight) {
+        this.sceneLight = sceneLight;
+    }
+
+    /**
+     * @return the fog
+     */
+    public Fog getFog() {
+        return fog;
+    }
+
+    /**
+     * @param fog the fog to set
+     */
+    public void setFog(Fog fog) {
+        this.fog = fog;
+    }
+
+}
---
+++ chapter16/src/main/java/org/lwjglb/engine/graph/lights/PointLight.java
@@ -0,0 +1,102 @@
+package org.lwjglb.engine.graph.lights;
+
+import org.joml.Vector3f;
+
+public class PointLight {
+
+    private Vector3f color;
+
+    private Vector3f position;
+
+    private float intensity;
+
+    private Attenuation attenuation;
+    
+    public PointLight(Vector3f color, Vector3f position, float intensity) {
+        attenuation = new Attenuation(1, 0, 0);
+        this.color = color;
+        this.position = position;
+        this.intensity = intensity;
+    }
+
+    public PointLight(Vector3f color, Vector3f position, float intensity, Attenuation attenuation) {
+        this(color, position, intensity);
+        this.attenuation = attenuation;
+    }
+
+    public PointLight(PointLight pointLight) {
+        this(new Vector3f(pointLight.getColor()), new Vector3f(pointLight.getPosition()),
+                pointLight.getIntensity(), pointLight.getAttenuation());
+    }
+
+    public Vector3f getColor() {
+        return color;
+    }
+
+    public void setColor(Vector3f color) {
+        this.color = color;
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position = position;
+    }
+
+    public float getIntensity() {
+        return intensity;
+    }
+
+    public void setIntensity(float intensity) {
+        this.intensity = intensity;
+    }
+
+    public Attenuation getAttenuation() {
+        return attenuation;
+    }
+
+    public void setAttenuation(Attenuation attenuation) {
+        this.attenuation = attenuation;
+    }
+
+    public static class Attenuation {
+
+        private float constant;
+
+        private float linear;
+
+        private float exponent;
+
+        public Attenuation(float constant, float linear, float exponent) {
+            this.constant = constant;
+            this.linear = linear;
+            this.exponent = exponent;
+        }
+
+        public float getConstant() {
+            return constant;
+        }
+
+        public void setConstant(float constant) {
+            this.constant = constant;
+        }
+
+        public float getLinear() {
+            return linear;
+        }
+
+        public void setLinear(float linear) {
+            this.linear = linear;
+        }
+
+        public float getExponent() {
+            return exponent;
+        }
+
+        public void setExponent(float exponent) {
+            this.exponent = exponent;
+        }
+    }
+}
