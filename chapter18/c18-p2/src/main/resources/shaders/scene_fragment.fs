a5a941ddaaefd1269a9fa91a4a780a390f18fd86
lwjglgamedev <ahbejarano@gmail.com>
2016-01-26 19:16:54 +0100

    Shadow acne correction

--- chapter18/c18-p2/src/main/resources/shaders/scene_fragment.fs
+++ chapter18/c18-p2/src/main/resources/shaders/scene_fragment.fs
@@ -166,7 +166,8 @@ float calcShadow(vec4 position)
     vec3 projCoords = position.xyz;
     // Transform from screen coordinates to texture coordinates
     projCoords = projCoords * 0.5 + 0.5;
-    if ( projCoords.z - 0.05 < texture(shadowMap, projCoords.xy).r ) 
+    float bias = 0.005;
+    if ( projCoords.z - bias < texture(shadowMap, projCoords.xy).r ) 
     {
         // Current fragment is not in shade
         shadowFactor = 0;
